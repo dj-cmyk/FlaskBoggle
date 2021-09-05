5f6019cf068e5dedd1e0e0f08fc7a8460fba41d7
diana <dianajewell@Dianas-MacBook-Air.local>
2021-09-05 16:55:46 -0400

    updated to check validity of words guessed and to keep track of current score on front end'

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, url_for, flash, session
+from flask import Flask, render_template, redirect, request, url_for, flash, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
 
@@ -10,6 +10,9 @@ debug = DebugToolbarExtension(app)
 
 
 boggle_game = Boggle()
+game_board = boggle_game.make_board(5)
+
+
 
 @app.route('/')
 def display_home_page():
@@ -18,8 +21,37 @@ def display_home_page():
 	return render_template('home.html')
 
 
-@app.route('/game')
+@app.route('/game', methods=['GET', 'POST'])
 def display_game_board():
-    '''start the game by showing the game board, guess form, and list of words already guessed'''
+    '''start the game by showing the game board, guess form, and space to keep track of guessed words. Also initializes session variables'''
 
-    return render_template('game.html')
+    session['game_board'] = game_board
+    session['guesses'] = []
+    session['current_score'] = 0
+    return render_template('game.html', game_board = game_board)
+
+
+@app.route('/guess/<word>', methods=['GET', 'POST'])
+def submit_guess(word):
+    '''user submits a guess and this is what happens'''
+    # do not refresh the page!
+
+    check_guess = boggle_game.check_valid_word(game_board, word)
+
+    if check_guess == 'ok':
+        scored_words = session['guesses']
+        scored_words.append((word, len(word)))
+        session['guesses'] = scored_words
+
+        session['current_score'] += len(word)
+        
+        print(session['guesses'], session['current_score'])
+
+    elif check_guess == 'not-on-board':
+        flash(f"Can't find {word} on this board")
+    else:
+        flash('try again')
+
+    return jsonify(check_guess)
+
+    
--- static/app.js
+++ static/app.js
@@ -1,2 +1,61 @@
 console.log('connected');
 
+const BASE_URL = 'http://127.0.0.1:5000/';
+
+const $guessBtn = $('#guess-btn');
+const $guessTxt = $('#guess-text');
+const guessed = $guessTxt.val();
+const $wordsGuessed = $('.guess-history');
+const $currScoreView = $('.curr-score')
+let currentScore = 0
+
+$guessBtn.on('click', async function(e){
+    e.preventDefault();
+    const sent_word = await send_guess_to_server($guessTxt.val());
+    const status_code = await get_response_from_server(sent_word);
+    console.log(status_code);
+
+    if(status_code === 'ok'){
+        update_scored_words(sent_word);
+        update_score(sent_word);
+        console.log('good word');
+    } else if (status_code === 'not-on-board'){
+        //flash message
+        console.log('not on board');
+    } else {
+        //flash other message
+        console.log('invalid word');
+    }
+    $guessTxt.val('');
+    return status_code
+})
+
+// send value to server using AXIOS
+async function send_guess_to_server(guess){
+    const response = await axios({
+        method: "POST",
+        url: `${BASE_URL}/guess/${guess}`,
+        data: guess
+      }); 
+      return response.config.data;
+}
+
+async function get_response_from_server(guess){
+    const res = await axios({
+        method: "GET",
+        url: `${BASE_URL}/guess/${guess}`,
+    })
+
+    return res.data;
+}
+
+function update_scored_words(guess){
+    newLi = `${guess}: ${guess.length}`;
+    $wordsGuessed.append(`<li>${newLi}</li>`);
+}
+
+function update_score(guess){
+    let word_score = guess.length
+    currentScore += word_score
+    $currScoreView.text(currentScore);
+}
