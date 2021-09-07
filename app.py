2ba52ec4fde23b6e1a058b4693d317e22a53ac5d
diana <dianajewell@Dianas-MacBook-Air.local>
2021-09-07 08:34:47 -0400

    fully functioning boggle game

--- app.py
+++ app.py
@@ -5,53 +5,66 @@ from boggle import Boggle
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_strng54321'
-#app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 
 boggle_game = Boggle()
-game_board = boggle_game.make_board(5)
-
 
 
 @app.route('/')
 def display_home_page():
-	'''display initial information about the game and button to start the game'''
+    '''display initial information about the game and button to start the game. Also initialize longer term session variables -> high score and number of games played'''
 
-	return render_template('home.html')
+    session['games_played'] = 1
+    session['high_score'] = 0
+
+    return render_template('home.html')
+
+
+@app.route('/init')
+def initialize_game():
+    '''initialize the current game variables -> game board, current score, and guesses'''
+    game_board = boggle_game.make_board(5)
+    session['game_board'] = game_board
+    session['current_score'] = 0
+    session['guesses'] = []
+
+    return redirect('/game')
 
 
 @app.route('/game', methods=['GET', 'POST'])
 def display_game_board():
-    '''start the game by showing the game board, guess form, and space to keep track of guessed words. Also initializes session variables'''
+    '''start the game by showing the game board, guess form, and space to keep track of guessed words'''
 
-    session['game_board'] = game_board
-    session['guesses'] = []
-    session['current_score'] = 0
+    game_board = session['game_board']
+    
     return render_template('game.html', game_board = game_board)
 
 
 @app.route('/guess/<word>', methods=['GET', 'POST'])
 def submit_guess(word):
-    '''user submits a guess and this is what happens'''
-    # do not refresh the page!
-
-    check_guess = boggle_game.check_valid_word(game_board, word)
-
-    if check_guess == 'ok':
-        scored_words = session['guesses']
-        scored_words.append((word, len(word)))
-        session['guesses'] = scored_words
-
-        session['current_score'] += len(word)
-        
-        print(session['guesses'], session['current_score'])
-
-    elif check_guess == 'not-on-board':
-        flash(f"Can't find {word} on this board")
-    else:
-        flash('try again')
-
-    return jsonify(check_guess)
-
+    '''user submits a guess which is collected by JS file and sent here. The word is checked for validity as a word and on the board, and then return result of validity check'''
     
+    game_board = session['game_board']
+    check_guess = boggle_game.check_valid_word(game_board, word)
+    scored_words = session['guesses']
+    if request.method == 'POST':
+        if check_guess == 'ok':
+            scored_words.append(word)
+            session['current_score'] += len(word)  
+            print(session['guesses'], session['current_score'])
+        return jsonify(check_guess)
+    else:
+        return jsonify(check_guess)
+
+
+@app.route('/endgame', methods=['GET', 'POST'])
+def game_over():
+    '''when reset button is pressed, the round score is checked to see if it is a new high score, and the number of games played is incremented. Then we redirect to initialize another round'''
+
+    if session['current_score'] > session['high_score']:
+        session['high_score'] = session['current_score']
+    session['games_played'] += 1
+    
+    return redirect('/init')
--- static/app.js
+++ static/app.js
@@ -1,34 +1,95 @@
 console.log('connected');
 
 const BASE_URL = 'http://127.0.0.1:5000/';
-
+const $startBtn = $('.start-btn');
 const $guessBtn = $('#guess-btn');
 const $guessTxt = $('#guess-text');
 const guessed = $guessTxt.val();
+const $guessMsgArea = $('.guess-messages');
 const $wordsGuessed = $('.guess-history');
-const $currScoreView = $('.curr-score')
-let currentScore = 0
+const $currScoreView = $('.curr-score');
+const $highScoreView = $('.high-score');
+const $timerDisplay = $('#timer');
+const $timerSeconds = $('#timer-seconds');
+const $timerBtn = $('.timer-btn');
+const $guessForm = $('.guess');
+const $scoredWords = $('.scored-words')
+let currentScore = 0;
+let highScore = 0;
+const wordArray = [];
+let num_games = 0;
+// const $resetDisplay = $('.reset-game');
+const $resetBtn = $('.reset-btn');
+const $endgameDisplay = $('.endgame-display');
 
-$guessBtn.on('click', async function(e){
-    e.preventDefault();
-    const sent_word = await send_guess_to_server($guessTxt.val());
-    const status_code = await get_response_from_server(sent_word);
-    console.log(status_code);
 
-    if(status_code === 'ok'){
-        update_scored_words(sent_word);
-        update_score(sent_word);
-        console.log('good word');
-    } else if (status_code === 'not-on-board'){
-        //flash message
-        console.log('not on board');
-    } else {
-        //flash other message
-        console.log('invalid word');
-    }
-    $guessTxt.val('');
-    return status_code
+let seconds = 60;
+$timerDisplay.hide();
+$guessForm.hide();
+$scoredWords.hide();
+$endgameDisplay.hide();
+
+
+
+$timerBtn.on('click', function(){
+    $timerBtn.hide();
+    $timerDisplay.show('slow');
+    timerRun();
+    $guessForm.show('slow');
+    $scoredWords.show('slow');
+    $guessBtn.on('click', async function(e){
+        await handle_guess(e);
+    })
 })
+
+function timerRun(){
+    let timerSample = setInterval(function(){
+        if(seconds > 0){
+            $timerSeconds.text(seconds);
+            seconds--;
+        } else {
+            $guessBtn.off()
+            clearInterval(timerSample);
+            $timerDisplay.hide('slow')
+            $guessForm.hide('slow');
+            $currScoreView.text(currentScore);
+            $guessMsgArea.hide('slow');
+            $endgameDisplay.show('slow');
+        } 
+    }, 1000)    
+}
+
+
+// handle guesses
+async function handle_guess(e){
+    e.preventDefault();
+    $guessMsgArea.hide('slow');
+
+    if(wordArray.includes($guessTxt.val())){
+        $guessMsgArea.html(`<p>You already guessed ${$guessTxt.val()}</p>`)
+        $guessMsgArea.show('slow');
+        $guessTxt.val('');
+        return 'already-guessed'
+    } else {
+        wordArray.push($guessTxt.val())
+        const sent_word = await send_guess_to_server($guessTxt.val());
+        const status_code = await get_response_from_server(sent_word);
+    
+        if(status_code === 'ok'){
+            update_scored_words(sent_word);
+            update_score(sent_word);
+        } else if (status_code === 'not-on-board'){
+            $guessMsgArea.html(`<p>Your guess is: ${status_code}</p>`)
+            $guessMsgArea.show('slow');
+        } else {
+            $guessMsgArea.html(`<p>Your guess is: ${status_code}</p>`)
+            $guessMsgArea.show('slow');
+        }
+        $guessTxt.val('');
+        return status_code
+    }
+    
+}
 
 // send value to server using AXIOS
 async function send_guess_to_server(guess){
@@ -50,8 +111,8 @@ async function get_response_from_server(guess){
 }
 
 function update_scored_words(guess){
-    newLi = `${guess}: ${guess.length}`;
-    $wordsGuessed.append(`<li>${newLi}</li>`);
+    newLi = `<b>${guess}:</b> ${guess.length}`;
+    $wordsGuessed.append(`<li class="words">${newLi}</li>`);
 }
 
 function update_score(guess){
@@ -59,3 +120,26 @@ function update_score(guess){
     currentScore += word_score
     $currScoreView.text(currentScore);
 }
+
+// TODO: manage end of game
+// number of games played (part of requirements)
+
+
+// function endGame(){
+//     console.log(currentScore, highScore)
+//     // num_games += 1;
+
+//     // if(currentScore > highScore){
+//     //     highScore = currentScore;
+//     //     $highScoreView.text(highScore);
+//     // }
+
+//     $endgameDisplay.show('slow');
+//     // $guessMsgArea.show('slow');
+//     // $resetDisplay.show('slow');
+//     currentScore = 0;
+//     $currScoreView.text(0);
+// }
+
+
+// // TODO: 
